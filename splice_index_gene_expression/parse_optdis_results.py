6c805daf1c2142ccfc446fa9dfdac67f4d6357fd
jakeyeung <jakeyeung@gmail.com>
2013-06-04 14:37:57 -0700

    parse_optdis_results.py -> creates full gene list from separate gene files.
    files_in_directory.py -> functions to accommodate
    parse_optdis_results.py

--- splice_index_gene_expression/parse_optdis_results.py
+++ splice_index_gene_expression/parse_optdis_results.py
@@ -7,12 +7,14 @@ Created on 2013-06-02
 
 import sys
 import os
+import csv
 from utilities import set_directories, files_in_directory
 
 
 # Set constants
 inputdir = 'input'
 outputdir = 'output'
+gene_column_index = 0    # First column of row in output contains gene names. 
 
 # Set directories
 mydirs = set_directories.mydirs(inputdir, outputdir)
@@ -23,14 +25,27 @@ def get_files_in_directory(module_fullpath):
     Get a list of files in a directory.
     '''
     filelist = os.listdir(module_fullpath)
+    print filelist
     return filelist
     
+def get_genes_from_file(filepath, initial_gene_list):
+    '''
+    From a filepath, read its file and return its genes.
     
+    initial_gene_list contains a starter list with which new
+    genes will be appended to the end. This makes this piece of code
+    loopable. 
+    ''' 
+    with open(filepath, 'rb') as file_obj:
+        filereader = csv.reader(file_obj, delimiter='\t')
+        for row in filereader:
+            initial_gene_list.append(row[0])
+    return initial_gene_list
 
 def get_genes_from_filelist(filelist):
     '''
-    The modules directory contains n number of files corresponding to
-    n number of subnetworks. 
+    Input: filelist is a list of filenames to be read and information
+    extracted. 
     
     Each subnetwork contains between 3 to 8?? number of genes. 
     
@@ -41,21 +56,31 @@ def get_genes_from_filelist(filelist):
     '''
     pass
     
-    
-    
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Module directory must be provided in command line.')
         sys.exit()
     module_directory = sys.argv[1]
-    
     module_fullpath = os.path.join(mydirs.outputdir, module_directory)
-    
+    output_dir = os.path.dirname(module_fullpath)
+    output_filename = sys.argv[2]
+    output_fullpath = os.path.join(output_dir, output_filename)
     optdis_results = files_in_directory.files_in_directory(module_fullpath)
     
-    print optdis_results.dir
-    print optdis_results.filelist
+    gene_list = optdis_results.extract_first_col_all_files(sortresults=True)
     
+    optdis_results.write_genes_to_file(output_fullpath)
+    
+    print('%s genes written to file.' %len(gene_list))
+    
+    '''
+    outputpath = os.path.join(module_fullpath, 'full_list.txt')
+    
+    with open(outputpath, 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter='\t')
+        for gene in gene_list:
+            writer.writerow([gene])
+    '''
     
     
--- splice_index_gene_expression/utilities/files_in_directory.py
+++ splice_index_gene_expression/utilities/files_in_directory.py
@@ -6,6 +6,7 @@ Created on 2013-06-02
 
 
 import os
+import csv
 
 
 class files_in_directory(object):
@@ -21,9 +22,30 @@ class files_in_directory(object):
         self.dir = directory
         self.filelist = os.listdir(directory)
         
-    def extract_first_col_all_files(self):
+    def extract_first_col_all_files(self, sortresults=False):
         '''
         Extract first column for each file in filelist.
         '''
-        pass
-        
+        gene_list = []
+        for f in self.filelist:
+            with open(os.path.join(self.dir, f), 'rb') as file_obj:
+                filereader = csv.reader(file_obj, delimiter='\t')
+                for row in filereader:
+                    genename = row[0]
+                    module_number = f
+                    gene_list.append((genename, module_number))
+        if sortresults == False:
+            self.gene_list = gene_list
+        elif sortresults == True:
+            self.gene_list = sorted(gene_list, key=lambda x: int(x[1]))
+        return gene_list
+    
+    def write_genes_to_file(self, output_fullpath):
+        with open(output_fullpath, 'wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter='\t')
+            for gene in self.gene_list:
+                genename = gene[0]
+                modulenumber = gene[1]
+                writer.writerow([genename, modulenumber])
+        return None
+                
