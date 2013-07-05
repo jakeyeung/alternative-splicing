1909ef24352dc70aba56331461e6adb804755b00
jakeyeung <jakeyeung@gmail.com>
2013-07-05 12:18:40 -0700

    Added plot histogram function in plots.py to accompany visualize_gene_exprs_distribution.py

--- splice_index_gene_expression/utilities/plots.py
+++ splice_index_gene_expression/utilities/plots.py
@@ -9,8 +9,9 @@ try:
     from igraph import Graph as graph
 except ImportError:
     print('igraph not found, ignoring...')
+import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.mlab as mlab
 from utilities import igraph_utilities, interactive_annotations
 
 
@@ -53,14 +54,34 @@ def plot_subnetwork_expression(x_vector, y_vector,
     '''
     Create bubble plot
     '''
+    
+    # font = {'weight' : 'normal','size'   : 33}
+    # matplotlib.rc('font', **font)
+    
     # Get set of color_vector
     color_set = list(set(color_vector))    # Two colors only :(
+    
+    # Begin, some code I added for the case of probe and gene exprs...
+    unique_modules = [9, 19, 22, 28, 32, 33, 35, 42, 44, 45, 46, 47, 49, 50]
+    marker_list = []
+    for l in ordered_labels:
+        if l in unique_modules and len(color_set) == 2:
+            marker_list.append('s')
+        else:
+            marker_list.append('o')
+    # End, some code I added.
+    
     # Plot data
     ax = plt.subplot(111)
-    ax.scatter(x_vector, y_vector, s=bubble_size, linewidth=2, 
-               edgecolor='w', c=color_vector, alpha=0.75)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
+    
+    for x, y, size, col, mark in zip(x_vector, y_vector, bubble_size, color_vector, marker_list):
+        ax.scatter(x, y, s=size, linewidth=2, 
+           edgecolor='w', c=col, marker=mark, alpha=0.75)
+        
+    # ax.scatter(x_vector, y_vector, s=bubble_size, linewidth=2, 
+    #            edgecolor='w', c=color_vector, marker='o', alpha=0.75)
+    ax.set_xlabel(xlabel, size=33)
+    ax.set_ylabel(ylabel, size=33)
     for x, y, lab in zip(x_vector, y_vector, ordered_labels):
         ax.text(x, y, lab, size=11, horizontalalignment='right', 
                 verticalalignment='top')
@@ -72,12 +93,15 @@ def plot_subnetwork_expression(x_vector, y_vector,
     p1 = plt.Circle((0, 0), radius=bubble_size[0], color=color_set[0])
     if len(color_set) == 2:
         p2 = plt.Circle((0, 0), radius=bubble_size[0], color=color_set[1])
-        plt.legend([p1, p2], ['No AS Detected', 'AS Detected'])
+        p3 = plt.Circle((0, 0), radius=bubble_size[0], color=color_set[0])
+        p4 = plt.Circle((0, 0), radius=bubble_size[0], color=color_set[0])
+        plt.legend([p1, p2, p3, p4], ['No AS Detected', 'AS Detected', 'Unique Modules', 'Non-Unique Modules'], prop={'size':33})
     elif len(color_set) == 1:
         pass
     else:
         sys.exit('Only 1 or 2 colors can be specified.')
-    plt.title(title)
+    plt.title(title, size=33)
+    plt.tick_params(labelsize=33)
     
     # Draw 45 degree line on graph. 
     xmin, xmax, ymin, ymax = plt.axis()
@@ -97,13 +121,20 @@ def plot_stacked_bar(count1, count2,
                      barplot_barwidths,
                      color_vector, xlabel, ylabel, title,
                      output_fullpath):
+    
+    font = {'weight' : 'normal','size'   : 33}
+    matplotlib.rc('font', **font)
+    
     '''
-    Seems to only work for barplot events of 3!
+    count1: count horizontally
+    count2: count horizontally.
+    Height of bar is count1[i] + count2[i]
     '''
-    ind = np.arange(len(count1))
+    ind = range(0, len(count1))
+    ind = [i + 0.5 for i in ind]
     
     count_list = [count1, count2]
-    iteration = [count2, [0, 0, 0]]
+    iteration = [count2, [0]*len(count2)]
     for count, category, color, bottom in zip(count_list, 
                                        barplot_categories, 
                                        color_vector, iteration):
@@ -117,28 +148,51 @@ def plot_stacked_bar(count1, count2,
     fig = plt.gcf()
     fig.set_size_inches(18.5,10.5)
     plt.savefig(output_fullpath)
+    print('Plot saved to %s' %output_fullpath)
+    plt.show()
     plt.clf()
     
-def plot_stacked_bar2(count1, count2,
-                     barplot_categories, barplot_events,
-                     barplot_barwidths,
-                     color_vector, xlabel, ylabel, title,
-                     output_fullpath):
-    '''
-    Repeat to do for barplot_events length of 2. 
-    '''
-    ind = range(0, len(count1))
-    plt.bar(ind, tuple(count1), width=barplot_barwidths, 
-            color=color_vector[0], label=barplot_categories[0])
-    plt.bar(ind, tuple(count2), width=barplot_barwidths,
-            color=color_vector[1], label=barplot_categories[1])
+def plot_line_graph(x, y, xlabel, ylabel, title):
+    plt.figure(1)
+    plt.plot(x, y)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
-    plt.legend()
-    plt.xticks([(i + barplot_barwidths/2.) for i in ind], tuple(barplot_events))
-    fig = plt.gcf()
-    fig.set_size_inches(18.5,10.5)
-    plt.savefig(output_fullpath)
-    plt.clf()
+    plt.show()
     
+def plot_histogram(x, xlabel, ylabel, title, nbins=30, normed=False, 
+                   facecolor='green', alpha=0.75, add_best_fit=False):
+    '''
+    From a list of values, create histogram. Requires specification of
+    number of xlabel, ylabel, title.
+    '''
+    # Initialize a plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # Plot histogram
+    _, bins, _ = ax.hist(x, nbins, normed=normed, facecolor=facecolor, alpha=alpha)
+    '''
+    # hist uses np.histogram under the hood to create 'n' and 'bins'.
+    # np.histogram returns the bin edges, so there will be 50 probability
+    # density values in n, 51 bin edges in bins and 50 patches.  To get
+    # everything lined up, we'll compute the bin centers
+    '''
+    bincenters = 0.5*(bins[1:]+bins[:-1])
+    # add a 'best fit' line for the normal PDF
+    if add_best_fit==True:
+        mu, sigma = 100, 15    # Constants to define for bestfit
+        y = mlab.normpdf( bincenters, mu, sigma)
+        ax.plot(bincenters, y, 'r--', linewidth=1)
+    elif add_best_fit==False:
+        pass
+    else:
+        sys.exit('add_best_fit must be True or False, %s found' %add_best_fit)
+    # Add axis labels
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    # ax.set_xlim(40, 160)
+    # ax.set_ylim(0, 0.03)
+    ax.grid(True)
+    
+    plt.show()
