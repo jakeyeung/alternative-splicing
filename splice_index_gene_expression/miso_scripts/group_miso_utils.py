7bf6e38ca6f2e2367c7a2ac42d76375166a87ccf
jakeyeung <jakeyeung@gmail.com>
2013-07-23 17:54:58 -0700

    Initial commit: a function for writing a combined header across samples.

---
+++ splice_index_gene_expression/miso_scripts/group_miso_utils.py
@@ -0,0 +1,87 @@
+'''
+Created on 2013-07-23
+
+@author: jyeung
+
+Some functions to help out group_miso_results.py
+'''
+
+import os
+import csv
+import re
+
+
+def write_headers_all_samples(sample_dir_names_list, main_dir, 
+                              chromo, miso_output, csv_write_obj):
+    '''
+    For given miso output file (.miso), find that .miso file from a list of
+    samples, then write a common header by keeping everything the same
+    but updating the percent_accept, count, and assigned_count.
+    '''
+    # Define constants found in header.
+    percent_accept_index = 5
+    counts_index = 7
+    assigned_counts_index = 8
+    # Initialize percent_accept, counts and assigned_counts
+    percent_accept_list = []
+    counts_00_list = []
+    counts_10_list = []
+    counts_01_list = []
+    counts_11_list = []
+    assigned_counts_0_list = []
+    assigned_counts_1_list = []
+    for samp_dir in sample_dir_names_list:
+        with \
+            open(os.path.join(main_dir, samp_dir, 
+                              chromo, miso_output), 'rb') as readfile:
+            
+            reader = csv.reader(readfile, delimiter='\t')
+            header = reader.next()
+            # Get value of certain elements in header. 
+            percent_str = header[percent_accept_index]
+            # Get only the percent by taking the float from right
+            # of equal sign.
+            percent_accept_list.append(float(percent_str.split('=')[1]))
+            
+            # get counts and use regex to extract the counts:
+            counts_str = header[counts_index]
+            assigned_str = header[assigned_counts_index]
+            # Find (0,0):, (1,0):, (0,1):, (1,1): and get integers after it:
+            for jpat, jlist, jstr, jtype in zip(['0,0', '1,0', 
+                                                 '0,1', '1,1',
+                                                 '0', '1'], 
+                                                [counts_00_list, 
+                                                 counts_10_list, 
+                                                 counts_01_list, 
+                                                 counts_11_list,
+                                                 assigned_counts_0_list,
+                                                 assigned_counts_1_list],
+                                                [counts_str]*4 + [assigned_str]*2,
+                                                ['c']*4 + ['ass_c']*2):
+                # Create regex expression to search
+                if jtype == 'c':
+                    reg_exprs = ''.join(['(?<=\(', jpat, '\)\:)\d+'])
+                else:
+                    reg_exprs = ''.join(['(?<=', jpat, '\:)\d+'])
+                match = re.search(reg_exprs, jstr)
+                # match = re.search('(?<=\(0,0\)\:)\d+', counts_str)
+                if match:
+                    try:
+                        jlist.append(int(match.group(0)))
+                    except ValueError:
+                        print('Error: could not convert %s to int.' %match.group(0))
+                else:
+                    jlist.append(0)
+    # Modify the last saved header with percent_accept, 
+    # counts, assigned_counts and write as first line.
+    # Modify percent_accept, counts and assigned_counts:
+    header[percent_accept_index] = 'percent_accept=%.2f' \
+        %(float(sum(percent_accept_list)) / len(percent_accept_list))
+    header[counts_index] = 'counts=(0,0):%s,(1,0):%s,(0,1):%s,(1,1):%s' \
+        %(sum(counts_00_list), sum(counts_10_list), 
+          sum(counts_01_list), sum(counts_11_list))
+    header[assigned_counts_index] = 'assigned_counts=0:%s,1:%s' \
+        %(sum(assigned_counts_0_list), sum(assigned_counts_1_list))
+    raw_input(header)
+    csv_write_obj.writerow(header)
+    return None
