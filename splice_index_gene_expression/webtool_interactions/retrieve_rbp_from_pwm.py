9f89c22787c4a1ba41dfa4a14cc6fbe55894784d
jakeyeung <jakeyeung@gmail.com>
2013-10-05 13:09:31 -0700

    init commit: tools for crawling web. Put some functions from retrieve_rbp_from_pwm.py to webtool_utilities

--- splice_index_gene_expression/webtool_interactions/retrieve_rbp_from_pwm.py
+++ splice_index_gene_expression/webtool_interactions/retrieve_rbp_from_pwm.py
@@ -10,97 +10,13 @@ Requires selenium 2.0 and BeautifulSoup 4.3.1!
 import sys
 import csv
 import os
-import urllib
-import urllib2
-import string
-try:
-    from bs4 import BeautifulSoup
-except ImportError:
-    from BeautifulSoup import BeautifulSoup
 from selenium import webdriver
-
-def selenium_test():
-    # Firefox Webdriver
-    driver = webdriver.Firefox()    # no need to set things up like Chrome.
-    
-    # Go to codepad.orgse
-    driver.get('http://codepad.org')
-     
-    # Select the Python language option
-    python_link = driver.find_elements_by_xpath("//input[@name='lang' and @value='Python']")[0]
-    python_link.click()
-     
-    # Enter some text!
-    text_area = driver.find_element_by_id('textarea')
-    text_area.send_keys("print 'Hello,' + ' World!'")
-     
-    # Submit the form!
-    submit_button = driver.find_element_by_name('submit')
-    submit_button.click()
-     
-    # Make this an actual test. Isn't Python beautiful?
-    assert "Hello, World!" in driver.get_page_source()
-     
-    # Close the browser!
-    driver.quit()
-
-def beautiful_soup_test():
-    user_agent = 'Mozilla/5 (Solaris 10) Gecko'
-    headers = { 'User-Agent' : user_agent }
-    values = {'s' : 'China' }
-    data = urllib.urlencode(values)
-    request = urllib2.Request("http://www.dict.cc/", data, headers)
-    response = urllib2.urlopen(request)
-    the_page = response.read()
-    pool = BeautifulSoup(the_page)
-    results = pool.findAll('td', attrs={'class' : 'td7nl'})
-    source = ''
-    translations = []
-    
-    for result in results:
-        word = ''
-        for tmp in result.findAll(text=True):
-            word = word + " " + unicode(tmp).encode("utf-8")
-        if source == '':
-            source = word
-        else:
-            translations.append((source, word))
-    
-    for translation in translations:
-        print "%s => %s" % (translation[0], translation[1])
-        
-def select_dropdown_menu(driver, menu_name, select_option):
-    '''
-    From menu name, select an option.
-    '''
-    # Create string for searching element.
-    jstr = "//select[@name='%s']/option[text()='%s']" % (menu_name, 
-                                                          select_option)
-    driver.find_element_by_xpath(jstr).click()
-    return jstr
-
-def find_textbox(driver, textbox_id):
-    input_element = driver.find_element_by_id(textbox_id)
-    return input_element
-
-def convert_tab_to_four_spaces(long_str):
-    '''
-    Convert str containing \t to four spaces.
-    '''
-    splittabs = long_str.split('\t')
-    fourspaces = '    '.join(splittabs)
-    return fourspaces
-
-def read_textfile_as_string(textfile):
-    '''
-    Reads textfile and returns an object that is 
-    easily insertable to selenium driver.
-    '''
-    with open(textfile, 'rU') as myfile:
-        mylines = myfile.readlines()
-        mylines_join = ''.join(mylines)
-        mylines_fourspaces = convert_tab_to_four_spaces(mylines_join)
-    return mylines_fourspaces
+from webtool_utilities import select_dropdown_menu, find_textbox, \
+    read_textfile_as_string, write_list_to_file, get_files_with_extension
+try:
+    from bs4 import BeautifulSoup    # linux
+except ImportError:
+    from BeautifulSoup import BeautifulSoup    # windows
 
 def extract_rbps_from_annotations(rbp_annotations):
     '''
@@ -162,25 +78,6 @@ def retrieve_rbp_results(pagesource, tag, rbp_list):
             rbp_results.append(r_stripped)
     return list(set(rbp_results))
 
-def write_list_to_file(list, output_filename):
-    '''
-    From a list, write to file, each list is its own row.
-    '''
-    with open(output_filename, 'wb') as myfile:
-        jwriter = csv.writer(myfile, delimiter='\t')
-        count = 0
-        for i in list:
-            jwriter.writerow([i])
-            count += 1
-    return count
-
-def get_files_with_extension(folder, extension):
-    '''
-    Look in folder, get all folders with extension, (e.g. .pwm files)
-    '''
-    file_list = [f for f in os.listdir(folder) if f.endswith(extension)]
-    return file_list
-
 def query_cisrbp_get_rbp(driver, website, menuname, select_option, 
                          textbox_id, input_lines, tag, rbp_list):
     # Use Selenium to insert data and retrieve pagesource.
@@ -240,13 +137,12 @@ def find_rbps_matching_motifs(pwm_folder, rbp_annotations, output_path):
     # Close
     driver.close()
     
-def main():
-    # beautiful_soup_test()
-    # selenium_test()
-    pwm_folder = sys.argv[1]
-    rbp_annotations = sys.argv[2]
-    output_path = sys.argv[3]
+def main(pwm_folder, rbp_annotations, output_path):
     find_rbps_matching_motifs(pwm_folder, rbp_annotations, output_path)
 
 if __name__ == '__main__':
-    main()
+    pwm_folder = sys.argv[1]
+    rbp_annotations = sys.argv[2]
+    output_path = sys.argv[3]
+    main(pwm_folder, rbp_annotations, output_path)
+    
---
+++ splice_index_gene_expression/webtool_interactions/webtool_utilities.py
@@ -0,0 +1,119 @@
+'''
+Created on 2013-10-04
+
+@author: jyeung
+'''
+
+import csv
+import os
+import urllib
+import urllib2
+from selenium import webdriver
+try:
+    from bs4 import BeautifulSoup    # linux
+except ImportError:
+    from BeautifulSoup import BeautifulSoup    # windows
+
+
+
+def selenium_test():
+    # Firefox Webdriver
+    driver = webdriver.Firefox()    # no need to set things up like Chrome.
+    
+    # Go to codepad.orgse
+    driver.get('http://codepad.org')
+     
+    # Select the Python language option
+    python_link = driver.find_elements_by_xpath("//input[@name='lang' and @value='Python']")[0]
+    python_link.click()
+     
+    # Enter some text!
+    text_area = driver.find_element_by_id('textarea')
+    text_area.send_keys("print 'Hello,' + ' World!'")
+     
+    # Submit the form!
+    submit_button = driver.find_element_by_name('submit')
+    submit_button.click()
+     
+    # Make this an actual test. Isn't Python beautiful?
+    assert "Hello, World!" in driver.get_page_source()
+     
+    # Close the browser!
+    driver.quit()
+
+def beautiful_soup_test():
+    user_agent = 'Mozilla/5 (Solaris 10) Gecko'
+    headers = { 'User-Agent' : user_agent }
+    values = {'s' : 'China' }
+    data = urllib.urlencode(values)
+    request = urllib2.Request("http://www.dict.cc/", data, headers)
+    response = urllib2.urlopen(request)
+    the_page = response.read()
+    pool = BeautifulSoup(the_page)
+    results = pool.findAll('td', attrs={'class' : 'td7nl'})
+    source = ''
+    translations = []
+    
+    for result in results:
+        word = ''
+        for tmp in result.findAll(text=True):
+            word = word + " " + unicode(tmp).encode("utf-8")
+        if source == '':
+            source = word
+        else:
+            translations.append((source, word))
+    
+    for translation in translations:
+        print "%s => %s" % (translation[0], translation[1])
+        
+def select_dropdown_menu(driver, menu_name, select_option):
+    '''
+    From menu name, select an option.
+    '''
+    # Create string for searching element.
+    jstr = "//select[@name='%s']/option[text()='%s']" % (menu_name, 
+                                                          select_option)
+    driver.find_element_by_xpath(jstr).click()
+    return jstr
+
+def find_textbox(driver, textbox_id):
+    input_element = driver.find_element_by_id(textbox_id)
+    return input_element
+
+def convert_tab_to_four_spaces(long_str):
+    '''
+    Convert str containing \t to four spaces.
+    '''
+    splittabs = long_str.split('\t')
+    fourspaces = '    '.join(splittabs)
+    return fourspaces
+
+def read_textfile_as_string(textfile):
+    '''
+    Reads textfile and returns an object that is 
+    easily insertable to selenium driver.
+    '''
+    with open(textfile, 'rU') as myfile:
+        mylines = myfile.readlines()
+        mylines_join = ''.join(mylines)
+        mylines_fourspaces = convert_tab_to_four_spaces(mylines_join)
+    return mylines_fourspaces
+
+def write_list_to_file(mylist, output_filename):
+    '''
+    From a mylist, write to file, each mylist is its own row.
+    '''
+    with open(output_filename, 'wb') as myfile:
+        jwriter = csv.writer(myfile, delimiter='\t')
+        count = 0
+        for i in mylist:
+            jwriter.writerow([i])
+            count += 1
+    return count
+
+def get_files_with_extension(folder, extension):
+    '''
+    Look in folder, get all folders with extension, (e.g. .pwm files)
+    '''
+    file_list = [f for f in os.listdir(folder) if f.endswith(extension)]
+    return file_list
