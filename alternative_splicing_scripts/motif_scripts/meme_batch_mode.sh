d8b1e8f8c3f03d012150e034b7ec52a7f5a2f1ca
jakeyeung <jakeyeung@gmail.com>
2013-11-08 11:45:33 -0800

    Runs both tomtom and meme together.

--- alternative_splicing_scripts/motif_scripts/meme_batch_mode.sh
+++ alternative_splicing_scripts/motif_scripts/meme_batch_mode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Argument = -mod oops|zoops|anr -nmotifs <nmotifs> -minw <minw> -maxw <maxw> 
+# Argument = -m oops|zoops|anr -n <nmotifs> -w <minw> -W <maxw> -e <evalthreshold (for MEME)> -q <qvalue_threshold (for ToMTom)	> -d <database> fasta_dir output_dir
 
 usage()
 {
@@ -10,32 +10,85 @@ This script runs MEME and TomTom in batch mode.
 
 OPTIONS:
    -h      Show this message
-   -mod    Distribution of motifs (oops|zoops|anr)
-   -nmotifs Number of motifs to find.
-   -minw   minimum width of a motif.
-   -maxw   max width of a motif.
+   -m    Distribution of motifs (oops|zoops|anr), default zoops
+   -n Number of motifs to find, default 10.
+   -w   minimum width of a motif, default 7.
+   -W   max width of a motif, default 8.
+   -e   E-value threshold for MEME motif discovery, default none.
+   -q   Q-value threshold for TomTom motif comparison, default 1.
+   -d   Database to match discoverd motifs, default is ray2013_rbp database.
 EOF
 }
 
-mod=
-nmotifs=
-minw=
-maxw=
-while getopts “:mod:nmotifs:minw:maxw” OPTION
+batch_meme()
+{
+# Run meme on all fasta files in directory.
+
+# Try to make directory for output
+mkdir $output_dir
+echo $fasta_dir
+cd $fasta_dir
+for f in `ls *.fasta`
+do
+	# Get filename without .fasta extension
+	basename="${f%%.*}"
+	# Only put -evt option if it is not empty.
+	if [ -z "$evt" ]
+	then 
+		meme $f -mod $mod -dna -nmotifs $nmotifs -minw $minw -maxw $maxw -o $output_dir/$basename &
+	else
+		meme $f -mod $mod -dna -nmotifs $nmotifs -minw $minw -maxw $maxw -evt $evt -o $output_dir/$basename &
+	fi
+done
+wait
+echo "All jobs looped for MEME"
+}
+
+batch_tomtom()
+{
+cd $fasta_dir
+# Run TomTom on all meme files (run after batch_meme)
+for f in `ls *.fasta`
+do
+	basename="${f%%.*}"
+	tomtom -o $output_dir/$basename/rbp_matches -thresh $thresh $output_dir/$basename/meme.txt $db &
+done
+wait
+echo "All jobs looped for TOMTOM"
+}
+
+# Get optional arguments...
+mod=zoops
+nmotifs=10
+minw=7
+maxw=8
+evt=
+thresh=1
+db="/home/jyeung/meme/db/motif_databases/ray2013_rbp.meme"
+while getopts “hm:n:w:W:q:d:e:” OPTION
 do
      case $OPTION in
-         mod)
+         m)
              mod=$OPTARG
              ;;
-         nmotifs)
+         n)
              nmotifs=$OPTARG
              ;;
-         minw)
+         w)
              minw=$OPTARG
              ;;
-         maxw)
+         W)
              maxw=$OPTARG
              ;;
+		 q)
+			 thresh=$OPTARG
+			 ;;
+		 d)
+			 db=$OPTARG
+			 ;;
+		 e)
+			 evt=$OPTARG
+			 ;;
          ?)
              usage
              exit
@@ -43,10 +96,23 @@ do
      esac
 done
 
-# if [[ -z $mod ]] || [[ -z $nmotifs ]] || [[ -z $minw ]] || [[ -z $maxw]]
-# then
-     # usage
-     # exit 1
-# fi
+# Get positional arguments...
+fasta_dir=${@:$OPTIND:1}
+output_dir=${@:OPTIND+1:1}
 
-echo $mod $nmotifs $minw $maxw
+# Tell user what options are used...
+echo "Fasta dir: $fasta_dir"
+echo "Output dir: $output_dir"
+echo "Distribution of motifs: $mod"
+echo "Number of motifs to discover: $nmotifs"
+echo "Minimum width of motifs: $minw"
+echo "Maximum width of motifs: $maxw"
+echo "Q value threshold for TomTom motif comparison: $thresh"
+echo "Database used for TomTom motif comparison: $db"
+
+# Discover motifs with MEME
+# Run meme on all fasta files in directory.
+
+batch_meme
+batch_tomtom
+
