38b0406ecc52482c27df27e93182f9fe95f84b5a
jakeyeung <jakeyeung@gmail.com>
2013-11-15 16:25:17 -0800

    init commit: gets median from a list.

---
+++ alternative_splicing_scripts/motif_scripts/utilities/stats_functions.py
@@ -0,0 +1,12 @@
+'''
+Created on 2013-11-15
+
+@author: jyeung
+'''
+
+def median(mylist):
+    sorts = sorted(mylist)
+    length = len(sorts)
+    if not length % 2:
+        return (sorts[length / 2] + sorts[length / 2 - 1]) / 2.0
+    return sorts[length / 2]
