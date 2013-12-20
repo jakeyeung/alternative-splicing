09fe99651fa713f6cffd9cf1d36a635253f96d5e
jakeyeung <jakeyeung@gmail.com>
2013-12-19 17:35:12 -0800

    init commit: converts dna to protein, takes into account all possible (annotated) reading frames.

---
+++ alternative_splicing_scripts/database_scripts/convert_dna_fasta_to_protein_fasta.py
@@ -0,0 +1,370 @@
+'''
+Created on 2013-12-19
+
+@author: jyeung
+
+Code adotped from cbayly
+'''
+
+
+import pickle
+import sys 
+import re
+import csv
+
+def get_chromosome_list(add_chr_prefix=False):
+    '''
+    Get chromosome names, 1 2 3 4 .. 22 X Y
+    
+    If you want chr1, chr2...  chrX, chrY, use
+    add_prefix = True, otherwise it's False
+    '''
+    chr_list = [str(i) for i in range(1, 23)] + ['X', 'Y']
+    
+    if add_chr_prefix == True:
+        chr_list = [''.join(['chr', chr_name]) for chr_name in chr_list]
+        
+    return chr_list
+
+def get_subkeys():
+    '''
+    For subkeys, we want to easily retrieve it by a function.
+    
+    That way we can access them without hardcoding constants.
+    '''
+    # def subkey names
+    reading_frame_str = 'reading_frame'
+    gene_name_str = 'gene_name'
+    exon_number_str = 'exon_number'
+    gene_id_str = 'gene_id'
+    transcript_id_str = 'transcript_id'
+    exon_id_str = 'exon_id'
+    return reading_frame_str, gene_name_str, exon_number_str, \
+            gene_id_str, transcript_id_str, exon_id_str
+            
+def extract_info_from_exon(exon_info, search=''):
+    '''
+    the 9th column in ensembl GTF file contains semi-colon separated
+    values that contains gene id, gene name, etc...
+    
+    Input the 9th column (as a string) to this function, ask for what to search,
+    and it will output the gene info you're searching for, removing any quotes
+    it may be surrounded by.
+    
+    exon_info_as_list looks like:
+    ['gene_id', '"ENSG00000142449"', 'transcript_id', '"ENST00000601739"', 'exon_num
+    ber', '"46"', 'gene_name', '"FBN3"', 'gene_biotype', '"protein_coding"', 'transc
+    ript_name', '"FBN3-002"', 'exon_id', '"ENSE00001663237"']
+    
+    Find index of search string, return the value of index + 1
+    '''
+    # remove semi colons, then split by space
+    exon_info_as_list = exon_info.replace(';', '').split(' ')
+    # search string of interest, find its index, 
+    # then return the value at index + 1
+    try:
+        index_of_interest = exon_info_as_list.index(search)
+    except ValueError:
+        print 'Could not find %s in %s' %(search, exon_info)
+        sys.exit()
+    info_of_interest = exon_info_as_list[index_of_interest + 1]
+    # Remove quotes from info of interest
+    info_of_interest = info_of_interest.replace('"', '')
+    return info_of_interest
+
+def make_dictionary(ensdatabase, ensdictionary):
+    ensdict = {}
+    f = open(ensdatabase, 'rb')            #inputfile1 = '100part_Homo_sapiens.GRCH37.72.gtf'
+    #    Grabs readframe, constructs chromosome location, constructs exon 'identifiers' (transcriptnumber, jtype(like "exon"), number).
+    
+    # Define index names
+    chr_index = 0    # chromosome, we want 1, 2, 3.. X and Y's only
+    feature_index = 2    # we want coding sequence only
+    exon_start_index = 3
+    exon_end_index = 4
+    strand_index = 6
+    reading_frame_index = 7
+    exon_info_index = 8
+    
+    reading_frame_str, \
+    gene_name_str, \
+    exon_number_str, \
+    gene_id_str, \
+    transcript_id_str, \
+    _ = get_subkeys()
+    
+    chr_list = get_chromosome_list()
+    
+    jreader = csv.reader(f, delimiter='\t')
+    
+    count = 0
+    for row in jreader:
+        chr_name = row[chr_index]
+        if chr_name not in chr_list:
+            continue
+        else:
+            reading_frame = row[reading_frame_index]
+            if reading_frame == '.':
+                continue
+            else:
+                # Build key with chr:start:end:strand
+                start = row[exon_start_index]
+                end = row[exon_end_index]
+                strand = row[strand_index]
+                
+                # Add chr to chr_name, 
+                # then create location chr:start:end:strand
+                # This matches MISO coordinates for cassette exons
+                chr_name_prefixed = ''.join(['chr', chr_name])
+                location = ':'.join([chr_name_prefixed, start, end, strand])
+                
+                # Get exon info from ensembl
+                exon_info = row[exon_info_index]
+                
+                # Parse exon info to get gene name, exon numb, and ids
+                gene_name = \
+                    extract_info_from_exon(exon_info, search=gene_name_str)
+                exon_number = \
+                    extract_info_from_exon(exon_info, search=exon_number_str)
+                gene_id = \
+                    extract_info_from_exon(exon_info, search=gene_id_str)
+                transcript_id = \
+                    extract_info_from_exon(exon_info, search=transcript_id_str)
+                # if location does not exist, then initialize with empty dictionary,
+                # then add subdic information as a list.
+                # if location exists
+                if location not in ensdict:
+                    # location not yet exist, we need to 
+                    # initialize a subdic
+                    # then initialize an empty list in each subkey
+                    # before we can append as a list.
+                    ensdict[location] = {}
+                    # init my loops
+                    for subkey, subvalue in \
+                    zip([reading_frame_str, gene_name_str, exon_number_str, 
+                         gene_id_str, transcript_id_str], 
+                        [reading_frame, gene_name, exon_number, gene_id, 
+                         transcript_id]):
+                        # since new dic, add empty list, then append
+                        ensdict[location][subkey] = []
+                else:
+                    # location exists,
+                    # I assume subvalues are in list form, do nothing
+                    # so I can simply 
+                    # append subvalues to subkeys
+                    pass
+                for subkey, subvalue in \
+                zip([reading_frame_str, gene_name_str, exon_number_str, 
+                     gene_id_str, transcript_id_str], 
+                    [reading_frame, gene_name, exon_number, gene_id, 
+                     transcript_id]):
+                    # since new dic, add empty list, then append
+                    ensdict[location][subkey].append(subvalue)
+                count += 1
+    f.close()
+    # save pickle object to file.
+    pkl = open(ensdictionary, 'wb')        #outputfile1 ='100_part_HS.pkl'            
+    pickle.dump(ensdict, pkl)
+    pkl.close()
+    return ensdict, count
+
+def openpkl(ensdictionary):
+    ensdict = pickle.load( open(ensdictionary, "rb") )
+    return ensdict
+
+def getgencode():
+    gencode = {
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+        'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
+        'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W',
+        'ata':'I', 'atc':'I', 'att':'I', 'atg':'M',
+        'aca':'T', 'acc':'T', 'acg':'T', 'act':'T',
+        'aac':'N', 'aat':'N', 'aaa':'K', 'aag':'K',
+        'agc':'S', 'agt':'S', 'aga':'R', 'agg':'R',
+        'cta':'L', 'ctc':'L', 'ctg':'L', 'ctt':'L',
+        'cca':'P', 'ccc':'P', 'ccg':'P', 'cct':'P',
+        'cac':'H', 'cat':'H', 'caa':'Q', 'cag':'Q',
+        'cga':'R', 'cgc':'R', 'cgg':'R', 'cgt':'R',
+        'gta':'V', 'gtc':'V', 'gtg':'V', 'gtt':'V',
+        'gca':'A', 'gcc':'A', 'gcg':'A', 'gct':'A',
+        'gac':'D', 'gat':'D', 'gaa':'E', 'gag':'E',
+        'gga':'G', 'ggc':'G', 'ggg':'G', 'ggt':'G',
+        'tca':'S', 'tcc':'S', 'tcg':'S', 'tct':'S',
+        'ttc':'F', 'ttt':'F', 'tta':'L', 'ttg':'L',
+        'tac':'Y', 'tat':'Y', 'taa':'*', 'tag':'*',
+        'tgc':'C', 'tgt':'C', 'tga':'*', 'tgg':'W'}
+    return gencode
+
+def get_prevline_rf(prevline, exon_of_interest, prev_location, ensdict):
+    chromoloc = re.findall(r'(?!<=range)chr\w*:\d*:\d*:[-+]1', prevline)         #identify exon locations in fasta file line
+    if chromoloc:
+        # exon1loc = chromoloc[0]
+        #exon2loc = chromoloc[1]
+        #exon3loc = chromoloc[2]
+        if exon_of_interest == '1':
+            unstripped_location = chromoloc[0]                                    #THIS INDEX SHOULD BE AN ARGUMENT so we can change it when we want
+        elif exon_of_interest == '2':
+            unstripped_location = chromoloc[1]
+        #    unstripped_location = chromoloc[1]
+        elif exon_of_interest == '3':
+            unstripped_location = chromoloc[2]
+        #    unstripped_location = chromoloc[2]
+        else:
+            print "only_3_possible_exons"
+
+        location = unstripped_location[3:][:-3]                                    #this is necessary to skip the last location, which spans the others.    
+        strand = unstripped_location[-2:]
+        rf = ''
+        print unstripped_location
+        print location
+        if location in ensdict:
+            print ensdict[location]
+            for g in ensdict[location]:
+                for h in ensdict[location][g]:
+                    if h =='.':
+                        if rf == '.' or rf == '':
+                            rf = h
+                        else:
+                            pass
+                    else:
+                        if location == prev_location:                    #where p = the previous location of a readframe
+                            # rf2 = h
+                            prev_location = location
+                        else:
+                            rf = h                
+                            prev_location = location 
+        elif location not in ensdict:
+            rf = ""
+        print rf
+        print strand
+        return rf, prev_location, strand
+    else:
+        pass
+        
+def translate(nucleotide_seq, reading_frame):
+    gencode = getgencode()
+    if reading_frame == '0':
+        nucleotide_seq = nucleotide_seq
+    elif reading_frame == '1':
+        nucleotide_seq = nucleotide_seq[1:]
+    elif reading_frame == '2':
+        nucleotide_seq = nucleotide_seq[2:]
+    elif reading_frame == '.':
+        pass
+    elif reading_frame == '':
+        pass
+    else:
+        pass
+    if reading_frame == '0' or reading_frame == '1' or reading_frame == '2':
+        amino_acid_seq = ''.join([gencode.get(nucleotide_seq[3*i:3*i+3], 'X') \
+                                    for i in range(len(nucleotide_seq)//3)])
+        return amino_acid_seq
+    else:
+        print 'Reading frame must be 0, 1 or 2.'
+        sys.exit()
+        
+def write_seq_to_file(myseq, mywritefile, maxlength=60):
+    '''
+    Takes a sequence, writes to writefile.
+    Breaks down sequence to new line every 60 
+    characters.
+    Input:
+        myseq: a long string, probably > 60
+        mywritefile: writefile object from open()
+    '''
+    # init counters
+    letter_count = 0
+    seq_list = []
+    for aa in myseq:
+        seq_list.append(aa)
+        letter_count = letter_count + 1
+        if letter_count % maxlength == 0:
+            # Max lenght reached, write file.
+            mywritefile.write(''.join(seq_list + ['\n']))
+            # reset counters
+            seq_list = []
+    # Done iterating, write remainders to file
+    # only if seq_list is not empty.
+    if len(seq_list) != 0:
+        mywritefile.write(''.join(seq_list + ['\n']))
+    return None
+
+def extract_location_from_miso_coordinate(miso_coordinates, exon_number):
+    '''
+    Given MISO coordinate (e.g.:
+    chr2:25650404:25650500:-@chr2:25642384:25642404:-@chr2:25611071:25611230:-
+    Return either 1st, 2nd or 3rd (depending on exon_number) coordinate range.
+    
+    Exon number should be an integer.
+    '''
+    # split by @
+    miso_coords_list = miso_coordinates.split('@')
+    
+    # Get exon number, since index starts at 0, subtract exon_number by 1.
+    return miso_coords_list[exon_number -1]
+
+def main():
+    ensdictionary = sys.argv[1]
+    dna_fasta = sys.argv[2]
+    try:
+        exon_isoform_number = int(sys.argv[3])    # exon 1, 2 or 3...
+    except ValueError:
+        print '3rd argument must be integer, eg 1 2 or 3 (exon number)'
+        sys.exit()
+    protein_fasta = sys.argv[4]
+    
+    if len(sys.argv) < 5:
+        print '4 arguments must be specified in command line.'
+        sys.exit()
+    
+    print 'Loading dictionary from %s' %ensdictionary
+    ensembl_dic = pickle.load(open(ensdictionary, "rb"))
+    print 'Loaded dictionary from %s' %ensdictionary
+    protein_fasta_file = open(protein_fasta, 'wb')
+    
+    print 'Reading DNA fasta files and translating to protein...'
+    
+    matchcount = 0
+    nomatch = 0
+    count = 0
+    with open(dna_fasta, 'rb') as dnafile:
+        for readcount, line in enumerate(dnafile):    
+            # alternates header and sequence.
+            # remove \n from end of each line
+            line = line.strip()
+            if line.startswith('>'):    # header of fasta...
+                miso_coordinate = line[1:]    # removes first '>' in header
+                location = \
+                    extract_location_from_miso_coordinate(miso_coordinate, 
+                                                          exon_isoform_number)
+                    
+                # Get reading frames (as a list) and get all possible
+                # translations.
+                # nothing we can do if location not in dic...
+                if location in ensembl_dic:
+                    dna_seq = dnafile.next()
+                    # Get non-redundant reading frames for iterating
+                    reading_frames = \
+                        list(set(ensembl_dic[location]['reading_frame']))
+                    # print 'Gene name: %s' %ensembl_dic[location]['gene_name']
+                    # print dna_seq
+                    for reading_frame in reading_frames:
+                        # print 'Reading frames: %s' %reading_frame
+                        amino_acid_seq = translate(dna_seq, reading_frame)
+    
+    
+if __name__ == '__main__':
+    main()
--- alternative_splicing_scripts/database_scripts/index_exon_info_to_pkl.py
+++ alternative_splicing_scripts/database_scripts/index_exon_info_to_pkl.py
@@ -6,161 +6,8 @@ Created on 2013-12-19
 Code adotped from cbayly
 '''
 
-
-import pickle
 import sys 
-import csv
-
-def get_chromosome_list(add_chr_prefix=False):
-    '''
-    Get chromosome names, 1 2 3 4 .. 22 X Y
-    
-    If you want chr1, chr2...  chrX, chrY, use
-    add_prefix = True, otherwise it's False
-    '''
-    chr_list = [range(1, 23)] + ['X', 'Y']
-    
-    if add_chr_prefix == True:
-        chr_list = [''.join(['chr', chr_name]) for chr_name in chr_list]
-        
-    return chr_list
-
-def get_subkeys():
-    '''
-    For subkeys, we want to easily retrieve it by a function.
-    
-    That way we can access them without hardcoding constants.
-    '''
-    # def subkey names
-    reading_frame_str = 'reading_frame'
-    gene_name_str = 'gene_name'
-    exon_number_str = 'exon_number'
-    gene_id_str = 'gene_id'
-    transcript_id_str = 'transcript_id'
-    exon_id_str = 'exon_id'
-    return reading_frame_str, gene_name_str, exon_number_str, \
-            gene_id_str, transcript_id_str, exon_id_str
-            
-def extract_info_from_exon(exon_info, search=''):
-    '''
-    the 9th column in ensembl GTF file contains semi-colon separated
-    values that contains gene id, gene name, etc...
-    
-    Input the 9th column (as a string) to this function, ask for what to search,
-    and it will output the gene info you're searching for, removing any quotes
-    it may be surrounded by.
-    
-    exon_info_as_list looks like:
-    ['gene_id', '"ENSG00000142449"', 'transcript_id', '"ENST00000601739"', 'exon_num
-    ber', '"46"', 'gene_name', '"FBN3"', 'gene_biotype', '"protein_coding"', 'transc
-    ript_name', '"FBN3-002"', 'exon_id', '"ENSE00001663237"']
-    
-    Find index of search string, return the value of index + 1
-    '''
-    # remove semi colons, then split by space
-    exon_info_as_list = exon_info.replace(';', '').split(' ')
-    # search string of interest, find its index, 
-    # then return the value at index + 1
-    try:
-        index_of_interest = exon_info_as_list.index(search)
-    except ValueError:
-        print 'Could not find %s in %s' %(search, exon_info)
-        sys.exit()
-    info_of_interest = exon_info_as_list[index_of_interest + 1]
-    # Remove quotes from info of interest
-    info_of_interest = info_of_interest.replace('"', '')
-    return info_of_interest
-
-def make_dictionary(ensdatabase, ensdictionary):
-    ensdict = {}
-    f = open(ensdatabase, 'rb')            #inputfile1 = '100part_Homo_sapiens.GRCH37.72.gtf'
-    pkl = open(ensdictionary, 'wb')        #outputfile1 ='100_part_HS.pkl'
-    #    Grabs readframe, constructs chromosome location, constructs exon 'identifiers' (transcriptnumber, jtype(like "exon"), number).
-    
-    # Define index names
-    chr_index = 0    # chromosome, we want 1, 2, 3.. X and Y's only
-    feature_index = 2    # we want coding sequence only
-    exon_start_index = 3
-    exon_end_index = 4
-    reading_frame_index = 7
-    exon_info_index = 8
-    
-    reading_frame_str, \
-    gene_name_str, \
-    exon_number_str, \
-    gene_id_str, \
-    transcript_id_str, \
-    _ = get_subkeys()
-    
-    chr_list = get_chromosome_list()
-    
-    jreader = csv.reader(f, delimiter='\t')
-    for row in jreader:
-        chr_name = row[chr_index]
-        if chr_name not in chr_list:
-            continue
-        else:
-            feature = row[feature_index]
-            if feature != 'CDS':
-                continue
-            else:
-                # Build key with chr:start:end
-                start = row[exon_start_index]
-                end = row[exon_end_index]
-                location = ':'.join([chr_name, start, end])
-                
-                # get reading frame
-                reading_frame = row[reading_frame_index]
-                
-                # Get exon info from ensembl
-                exon_info = row[exon_info_index]
-                
-                # Parse exon info to get gene name, exon numb, and ids
-                gene_name = \
-                    extract_info_from_exon(exon_info, search=gene_name_str)
-                exon_number = \
-                    extract_info_from_exon(exon_info, search=exon_number_str)
-                gene_id = \
-                    extract_info_from_exon(exon_info, search=gene_id_str)
-                transcript_id = \
-                    extract_info_from_exon(exon_info, search=transcript_id_str)
-                '''
-                exon_id = \
-                    extract_info_from_exon(exon_info, search=exon_id_str)
-                '''
-                # if location does not exist, then initialize with empty dictionary,
-                # then add subdic information as a list.
-                # if location exists
-                if location not in ensdict:
-                    # location not yet exist, we need to 
-                    # initialize a subdic
-                    # then initialize an empty list in each subkey
-                    # before we can append as a list.
-                    ensdict[location] = {}
-                    # init my loops
-                    for subkey, subvalue in \
-                    zip([reading_frame_str, gene_name_str, exon_number_str, 
-                         gene_id_str, transcript_id_str], 
-                        [reading_frame, gene_name, exon_number, gene_id, 
-                         transcript_id]):
-                        # since new dic, add empty list, then append
-                        ensdict[location][subkey] = []
-                else:
-                    # location exists,
-                    # I assume subvalues are in list form, do nothing
-                    # so I can simply 
-                    # append subvalues to subkeys
-                    pass
-                for subkey, subvalue in \
-                zip([reading_frame_str, gene_name_str, exon_number_str, 
-                     gene_id_str, transcript_id_str], 
-                    [reading_frame, gene_name, exon_number, gene_id, 
-                     transcript_id]):
-                    # since new dic, add empty list, then append
-                    ensdict[location][subkey].append(subvalue)
-    f.close()            
-    pickle.dump(ensdict, pkl)
-    pkl.close()
+from convert_dna_fasta_to_protein_fasta import make_dictionary
 
 def main():
     ensdatabase = sys.argv[1]
@@ -171,8 +18,8 @@ def main():
         sys.exit()
     
     print 'Making dictionary from %s' %ensdatabase
-    make_dictionary(ensdatabase, ensdictionary)
-    print 'Dictionary saved to %s' %ensdictionary
+    _, count = make_dictionary(ensdatabase, ensdictionary)
+    print 'Dictionary with %s entries saved to %s' %(count, ensdictionary)
     
 if __name__ == '__main__':
     main()
