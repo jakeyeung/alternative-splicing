8d24375e40345da8bf2f5ec8f8050ce43dcc77ed
jakeyeung <jakeyeung@gmail.com>
2014-04-02 14:11:53 -0700

    Calculates background gene list matches explicitly.

--- alternative_splicing_scripts/miso_scripts/match_gene_list_fishers_test.py
+++ alternative_splicing_scripts/miso_scripts/match_gene_list_fishers_test.py
@@ -39,6 +39,13 @@ def main():
     parser.add_option('-N', '--n_bg_total_genes', dest='n_bg_total',
                       default=8863,
                       help='Number of background genes. Default 8863 for SE.')
+    parser.add_option('-B', '--background_file', dest='bg_file',
+                      default=None,
+                      help='Background file of gene names. Ignores -n and -N if used.')
+    parser.add_option('-b', '--background_colname', dest='bg_colname',
+                      default='gsymbol',
+                      help='To extract gene names from bg file, need to know '\
+                        'colname corresponding to gene name. Default "gsymbol".')
     # Parse options
     (options, args) = parser.parse_args()
     
@@ -47,13 +54,21 @@ def main():
     
     colname1 = options.colname1
     colname2 = options.colname2
-    n_bg_matched = int(options.n_bg_matched)
-    n_bg_total = int(options.n_bg_total)
+    if options.bg_file is None:
+        n_bg_matched = int(options.n_bg_matched)
+        n_bg_total = int(options.n_bg_total)
+    else:
+        bg_file = options.bg_file
+        bg_colname = options.bg_colname
     filename3 = options.filename3
     colname3 = options.colname3
     
     gene_list1 = get_events(filename1, colname1)
     gene_list2 = get_events(filename2, colname2)
+    if options.bg_file is not None:
+        print 'Extracting genes from background file: %s' %bg_file
+        bg_gene_list = get_events(bg_file, bg_colname)
+        print 'Done.'
     if filename3 is not None:
         print 'Filename3 supplied: will use intersection '\
             'of\n%s\nand\n%s\n as genelist.' %(filename1, filename3)
@@ -64,6 +79,11 @@ def main():
     gene_list2 = set([gene.lower() for gene in gene_list2])
     if filename3 is not None:
         gene_list3 = set([gene.lower() for gene in gene_list3])
+    if options.bg_file is not None:
+        bg_gene_list = set([gene.lower() for gene in bg_gene_list])
+        # only consider ones not in gene_list1 because that is
+        # what would happen if you were to draw without replacement.
+        bg_gene_list = bg_gene_list - gene_list1
     
     # if gene_list3 exists, find intersection with genelist1, then rename it
     # as genelist1.
@@ -76,13 +96,26 @@ def main():
         '''
         gene_list1 = gene_list1 & gene_list3
     
-    n_in_reg = len(gene_list2 & gene_list1)
+    matched_genes = gene_list2 & gene_list1
+    n_in_reg = len(matched_genes)
     n_not_in_reg = len(gene_list1) - n_in_reg
     
-    n_in_reg_bg = n_bg_matched
-    n_not_in_reg_bg = n_bg_total - n_bg_matched
+    if options.bg_file is None:
+        n_in_reg_bg = n_bg_matched
+        n_not_in_reg_bg = n_bg_total - n_bg_matched
+    else:
+        print 'Manually calculating matches to bg genes.'
+        # manually calculate genes in background
+        # since we have the gene list.
+        bg_matched_genes = gene_list2 & bg_gene_list
+        n_in_reg_bg = len(bg_matched_genes)
+        n_not_in_reg_bg = len(bg_gene_list) - n_in_reg_bg
     
-    _, pvalue = stats.fisher_exact([[n_in_reg, n_not_in_reg], [n_in_reg_bg, n_not_in_reg_bg]])
+    print 'Fishers exact inputs: \nmatches to list=%s, \nnon matches '\
+        'to list=%s, \nmatches to bg=%s, \nnon matches to bg=%s' \
+        %(n_in_reg, n_not_in_reg, n_in_reg_bg, n_not_in_reg_bg)
+    _, pvalue = stats.fisher_exact([[n_in_reg, n_not_in_reg], [n_in_reg_bg, n_not_in_reg_bg]],
+                                   alternative='greater')
     
     print 'Out of %s genes in genelist1, %s matched to genelist2.\nPvalue: %s' \
         %(len(gene_list1), n_in_reg, pvalue)
