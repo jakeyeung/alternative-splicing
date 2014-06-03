3a558ee1eed04da820513fa9d8a36591b948d4c8
jakeyeung <jakeyeung@gmail.com>
2014-06-03 09:30:16 -0700

    exclude directories that do not include inclusion or exclusion

--- alternative_splicing_scripts/motif_scripts/parse_shuffled_unshuffled_evalues.py
+++ alternative_splicing_scripts/motif_scripts/parse_shuffled_unshuffled_evalues.py
@@ -17,6 +17,27 @@ import csv
 from optparse import OptionParser
 from utilities import get_evalues
 
+def filter_dirs(dirs_unfiltered, filterstr='sion'):
+    '''
+    Given a list of directory names, filter out ones that
+    do not contain filterstr. This method was written to filter out
+    directories that did not end in either "inclusion" or "exclusion" 
+    '''
+    dirs_filtered = []
+    for d in dirs_unfiltered:
+        if d.endswith(filterstr):
+            dirs_filtered.append(d)
+    return dirs_filtered
+
+def write_evals_to_file(evals, writeobj, groupname=''):
+    '''
+    Given a list of evalues, write them to output
+    '''
+    count = 0
+    for count, eval in enumerate(evals):
+        writeobj.writerow([groupname, eval])
+    return count
+
 def main():
     usage = 'usage: %prog motif_dir shuffled_dir output_file.parsed'
     parser = OptionParser(usage=usage)
@@ -31,8 +52,11 @@ def main():
     write_out_file = args[2]
     
     # Get list of directories containing meme outputs.
-    non_null_dirs = os.listdir(non_null_rootdir)
-    null_dirs = os.listdir(null_rootdir)
+    non_null_dirs_unfiltered = os.listdir(non_null_rootdir)
+    null_dirs_unfiltered = os.listdir(null_rootdir)
+    
+    non_null_dirs = filter_dirs(non_null_dirs_unfiltered)
+    null_dirs = filter_dirs(null_dirs_unfiltered)
     
     # Init write out file
     writefile = open(write_out_file, 'wb')
@@ -56,12 +80,13 @@ def main():
     # write header
     mywriter.writerow(['Group', 'Motif E-Value'])
     writecount = 0
-    for non_null_eval, null_eval in zip(non_null_evalues, null_evalues):
-        # Call non-nulls "Sequences of Interest"
-        # Call nulls "Shuffled Sequences"
-        mywriter.writerow(['Sequences of Interest', non_null_eval])
-        mywriter.writerow(['Shuffled Sequences', null_eval])
-        writecount += 2
+    
+    writecount += write_evals_to_file(non_null_evalues, 
+                                      mywriter, 
+                                      'Sequences of Interest')
+    writecount += write_evals_to_file(null_evalues, 
+                                      mywriter, 
+                                      'Shuffled Sequences')
     
     print '%s evalues written to file: %s' %(writecount, write_out_file)
     writefile.close()
