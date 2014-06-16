5f5a0c841b9decdb41ca199001514f32b3bd9c83
jyeung <jakeyeung@gmail.com>
2014-06-16 16:13:04 -0700

    Change extension to *.summary, so the code runs on constitutive exons without an inclusion or exclusion prefix

--- alternative_splicing_scripts/motif_scripts/compare_anchor_input_lengths.py
+++ alternative_splicing_scripts/motif_scripts/compare_anchor_input_lengths.py
@@ -3,10 +3,10 @@ Created on 2014-03-31
 
 @author: jyeung
 
-Reads as input two (or more) anchor.summary files 
+Reads as input two (or more) anchor.summary files
 (created from 2 instances of run_anchor_batch.py)
 and plots a density chart comparing amino acid sequence
-lengths between the 2 summary files. 
+lengths between the 2 summary files.
 '''
 
 import sys
@@ -20,7 +20,7 @@ def get_aa_length_from_anchor_file(jfile):
     Reads file, expects a header and looks for
     column "amino_acid_sequence" and extracts
     length of each amino acid sequence.
-    
+
     Returns list of lengths.
     '''
     colname = 'amino_acid_sequence'
@@ -39,7 +39,7 @@ def main():
         '2) Directory to which to compare containing .summary files (control)\n'
     parser = OptionParser(usage=usage)
     (_, args) = parser.parse_args()
-    
+
     if len(args) != 2:
         print 'Requires 2 arguments to be specified in command line'
         print usage
@@ -47,15 +47,15 @@ def main():
     # parse args
     dir0 = args[0]
     dir1 = args[1]
-    
-    # get all files containing *sion.summary in each directory. Those are 
+
+    # get all files containing *.summary in each directory. Those are
     # anchor input files.
-    ext = 'sion.summary'
+    ext = '.summary'
     anchor_files0 = \
         [os.path.join(dir0, f) for f in os.listdir(dir0) if f.endswith(ext)]
     anchor_files1 = \
         [os.path.join(dir1, f) for f in os.listdir(dir1) if f.endswith(ext)]
-    
+
     # Read each file, retrieving the lengths of each amino acid sequence
     aa_lengths0 = []
     aa_lengths1 = []
@@ -63,7 +63,7 @@ def main():
         aa_lengths0 += get_aa_length_from_anchor_file(file0)
     for file1 in anchor_files1:
         aa_lengths1 += get_aa_length_from_anchor_file(file1)
-    
+
     for id, aa in zip(['xeno', 'control'], [aa_lengths0, aa_lengths1]):
         print 'Mean for %s' %id
         print sum(aa)/float(len(aa))
@@ -71,16 +71,16 @@ def main():
         print sorted(aa)[len(aa)//2]
         print '[min,max] for %s' %id
         print '[%s,%s]' %(min(aa), max(aa))
-    plot_functions.plot_density([aa_lengths0, aa_lengths1], 
+    plot_functions.plot_density([aa_lengths0, aa_lengths1],
                                 mytitle='Density plot of exon lengths',
                                 xlabel='Nucleotide length',
                                 ylabel='Density',
-                                labels_lists=['Cassette Exons', 
-                                             'Constitutively Exons'],
+                                labels_lists=['Cassette Exons',
+                                             'Constitutive Exons'],
                                 smoothness=0.1,
                                 legend_pos=1,
                                 ymin=0, ymax=0.025,
                                 xmin=0, xmax=200)
-    
+
 if __name__ == '__main__':
     main()
