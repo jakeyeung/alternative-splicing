aef62e21ba9efb7bebf2aad52c0ae5c1d0f2bb7c
jyeung <jakeyeung@gmail.com>
2014-06-23 15:38:22 -0700

    Fix import issue

--- alternative_splicing_scripts/motif_scripts/plot_anchor_results_comparison.py
+++ alternative_splicing_scripts/motif_scripts/plot_anchor_results_comparison.py
@@ -8,14 +8,14 @@ import sys
 import csv
 from optparse import OptionParser
 from scipy.stats.stats import fisher_exact
-from motif_scripts.utilities.plot_functions import plot_barplot
+from utilities.plot_functions import plot_barplot
 import matplotlib.pyplot as plt
 
 def count_anchor_results(results_file, colname='binding_regions'):
     '''
     Read results file, count rows containing
     binding_regions.
-    
+
     Return rows containing binding regions and
     total number of rows counted.
     '''
@@ -28,13 +28,13 @@ def count_anchor_results(results_file, colname='binding_regions'):
             if value is not '':
                 value_count += 1
     return value_count, row_count
-    
+
 def main():
     usage = 'usage: %prog anchor_results.txt anchor_results_null.txt\n'\
         'Requires two input arguments:\n'\
         '1) Interesting anchor results, output from run_anchor_batch.py\n'\
         '2) Null anchor results, output from run_anchor_batch.py\n'
-    parser = OptionParser(usage=usage)    
+    parser = OptionParser(usage=usage)
     parser.add_option('-1', '--exon_label1', dest='exon_label1',
                       default='Exon label 1',
                       help='Exon label of anchor_results.txt.')
@@ -54,42 +54,42 @@ def main():
     exon_label1 = options.exon_label1
     exon_label2 = options.exon_label2
     mytitle = options.title
-    
+
     # init dic with keys and empty lists
     anchor_dic = {}
     for key in ['binding', 'non_binding', 'total']:
         anchor_dic[key] = []
-    
+
     for results in [anchor_results_path, anchor_results_null_path]:
         binding_count, total_count = count_anchor_results(results)
         non_binding_count = total_count - binding_count
-        for key, val in zip(['binding', 'non_binding', 'total'], 
+        for key, val in zip(['binding', 'non_binding', 'total'],
                             [binding_count, non_binding_count, total_count]):
             anchor_dic[key].append(val)
-    
+
     oddsratio, pvalue = \
         fisher_exact([anchor_dic['binding'], anchor_dic['non_binding']])
-    
+
     print 'oddsratio: %s\npvalue: %s' %(oddsratio, pvalue)
-    
+
     # plot distributions (from plot_meme_motif_null_comparison.py)
     mylabels = [exon_label1, exon_label2]
     # Plot bargraphs
     frac_binding = float(anchor_dic['binding'][0]) / anchor_dic['total'][0]
     frac_binding_null = float(anchor_dic['binding'][1]) / anchor_dic['total'][1]
     myvals = [frac_binding, frac_binding_null]
-    plot_barplot(myvals, mytitle, mylabels, 
-                 ylabel='Fraction predicted binding regions', 
+    plot_barplot(myvals, mytitle, mylabels,
+                 ylabel='Fraction predicted binding regions',
                  mytext1="%i/%i" \
-                    %(anchor_dic['binding'][0], 
+                    %(anchor_dic['binding'][0],
                       anchor_dic['total'][0]),
-                  mytext2='%i/%i' %(anchor_dic['binding'][1], 
+                  mytext2='%i/%i' %(anchor_dic['binding'][1],
                                     anchor_dic['total'][1]),
                   mytext3="*Fisher's Exact Test\nP-value=%.2e" %pvalue,
                   ymin=0,
                   ymax=1,
                   width=0.5)
     plt.show()
-    
+
 if __name__ == '__main__':
     main()
