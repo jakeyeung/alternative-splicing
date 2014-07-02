db0dea7618ebda82938146f856214b9876654b7a
jyeung <jakeyeung@gmail.com>
2014-07-02 11:40:47 -0700

    Display required positional arguments if # positional arguments not met

--- alternative_splicing_scripts/database_scripts/index_exon_info_to_pkl.py
+++ alternative_splicing_scripts/database_scripts/index_exon_info_to_pkl.py
@@ -6,20 +6,22 @@ Created on 2013-12-19
 Code adotped from cbayly
 '''
 
-import sys 
+import sys
 from create_dna_protein_summary_file import make_dictionary
 
 def main():
     ensdatabase = sys.argv[1]
     ensdictionary = sys.argv[2]
-    
+
     if len(sys.argv) < 3:
-        print '3 arguments must be specified in command line.'
+        print '2 arguments must be specified in command line.'
+        print '1) .gtf file of gene annotations from ensembl'
+        print '2) output .pkl file'
         sys.exit()
-    
+
     print 'Making dictionary from %s' %ensdatabase
     _, count = make_dictionary(ensdatabase, ensdictionary)
     print 'Dictionary with %s entries saved to %s' %(count, ensdictionary)
-    
+
 if __name__ == '__main__':
     main()
