31feec3e5091420727f270194cfcdfd18e7877b8
jyeung <jakeyeung@gmail.com>
2014-07-10 12:24:16 -0700

    Add input and output arguments instead of hardcoded in script.

--- R/plot_bubble_db_rbps.R
+++ R/plot_bubble_db_rbps.R
@@ -3,16 +3,21 @@
 # Plot only RBPs in database.
 # March 21 2014
 
+# Args --------------------------------------------------------------------
+
+args <- commandArgs(trailingOnly=TRUE)
+vpc_beltran_path <- args[1]
+fname <- args[2]
+
 # Libraries ---------------------------------------------------------------
 
 library(ggplot2)
-library("extrafont")    # for Arial font
 library(grid)
-
+library("extrafont")
 
 # LoadFile ----------------------------------------------------------------
 
-vpc_beltran_path <- "G:/jyeung/projects/alternative_splicing/output/xenograft_exprs/pvals_db_rbps.three_cohorts_combined.custom.txt"
+# vpc_beltran_path <- "G:/jyeung/projects/alternative_splicing/output/xenograft_exprs/pvals_autophagy.three_cohorts_combined.txt"
 vpc_beltran_data <- read.table(vpc_beltran_path, header=TRUE)
 
 
@@ -24,7 +29,7 @@ str(vpc_beltran_data)
 
 # Filter ------------------------------------------------------------------
 
-fname='G:/jyeung/projects/alternative_splicing/figs/bubble_plot_genes_in_db_only_final_with_RBFOX1_wide3.eps'
+# fname='G:/jyeung/projects/alternative_splicing/figs/bubble_plot_autophagy3.eps'
 loadfonts(quiet=TRUE)    # make things arial
 postscript(fname, height=8.83, width=10.83,
 #postscript(fname, height=6, width=15,    # for poster
@@ -33,19 +38,19 @@ postscript(fname, height=8.83, width=10.83,
            onefile=FALSE)
 
 g <- ggplot(vpc_beltran_data, aes(x=log10_p_value.beltran, y=log10_p_value.vpc, size=abs_fold_change, fill=fc_direction), legend=FALSE) +
-        geom_point(colour='black', shape=21) + scale_area(range=c(1,15)) + 
+        geom_point(colour='black', shape=21) + scale_size_area(c(1,15)) + 
         geom_text(data=subset(vpc_beltran_data, abs_fold_change > 4 | log10_p_value.vpc > 1.5 | log10_p_value.beltran > 3), 
-                  aes(label=gene),
+                  aes(label=gene, y=log10_p_value.vpc+0.03),
                   size=4) +
         scale_x_continuous(name=expression(paste('-log'[10], ' BH-Adj P-Value: Beltran Cohort'))) +
         scale_y_continuous(name=expression(paste('-log'[10], ' BH-Adj P-Value: VPC Cohort'))) +
         #geom_text(size=vpc_beltran_data$abs_fold_change, color='black')+
         labs(size=expression(paste('log'[2], ' FC :331R vs 331')), color="Fold Change (FC)") + 
         theme_bw(base_size = 25) %+replace%
-            theme(legend.justification=c(1,0), legend.position=c(1,0)) +
+            theme(legend.justification=c(1,0), legend.position=c(1,0))
         # put legend at bottom, horizontal
         # opts(legend.position = "right")    # for poster
-        opts(legend.direction = "horizontal", legend.position = "bottom")
+        # opts(legend.direction = "horizontal", legend.position = "bottom")
 g
 dev.off()
 print(paste(c('Figure output:', fname)))
